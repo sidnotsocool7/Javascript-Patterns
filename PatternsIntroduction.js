4f0cf33db3620e4c663b2818756a85cf33afe6ea
sidnotsocool7 <56714532+sidnotsocool7@users.noreply.github.com>
2019-11-19 14:00:29 +0000

    Create PatternsIntroduction.js

---
+++ PatternsIntroduction.js
@@ -0,0 +1,5 @@
+/** 
+* Native Object - Defined in ECMAScript standard. Array, Date or user-defined object.
+* Host Object - Defined by host environment(browser env). Ex.- window and all DOM objects.
+* Use JsLint and console object.
+*/
